1ede20489a59bc91047e49ca6e2c092b326e8aaf
justinxu421 <justinx@stanford.edu>
2020-12-13 01:53:46 -0500

    clean up

--- advent_2020/day13.py
+++ advent_2020/day13.py
@@ -2,23 +2,18 @@ from collections import Counter, defaultdict
 from utils import print_d
 
 def p1(x, a):
-    buses = []
-    for bus in a:
-        if bus != 'x':
-            buses.append(int(bus))
+    buses = [int(bus) for bus in a if bus != 'x']
 
-    min_ = 999999999999999999999
-    min_bus = None
-    for bus in buses:
-        new = ((int(x)+1) // bus + 1) * bus
+    def min_div( bus):
+        return bus, ((int(x)+1) // bus + 1) * bus
         
-        if new < min_:
-            min_ = new
-            min_bus = bus
+    div_list = list(map(min_div, buses))
+    div_list.sort(key = lambda x: x[0])
     
-    print(min_, x, min_bus)
+    min_bus, min_  = div_list[0]
     return (min_ - x) * min_bus
 
+# ensure our solution is valid
 def check_valid(buses, x):
     for i, bus in buses:
         if x % bus != i:
@@ -26,26 +21,13 @@ def check_valid(buses, x):
     return True
 
 def p2(a):
-    buses = []
-    for i, bus in enumerate(a):
-        if bus != 'x':
-            buses.append((-i % int(bus), int(bus)))
-    
-    prod = 1
-    for i, bus in buses:
-        prod *= bus
-    
+    # get the remainders and divisors and sort
+    buses = [(-i % int(bus), int(bus)) for i, bus in enumerate(a) if bus != 'x']
     buses.sort(key = lambda x: x[1])
-    print(prod)
-    print(buses)
     
-    increment = 1
-
     r1, m1 = buses[-1]
-    r1 = r1 % m1
     while len(buses) >= 2:
         r2, m2 = buses[-2]
-        print(r1, m1, r2, m2)
         
         while r1 % m2 != r2:
             r1 += m1
@@ -53,19 +35,13 @@ def p2(a):
         buses.pop()
 
     print(check_valid(buses, r1))
-    print(check_valid(buses, 1068781))
     return r1
-
-### insert how to parse line
-def parse_line(line):
-    return list(line.strip())
-    #return line[0], int(line.strip()[1:])
 
 def main():
     with open('input13.txt') as f:
         x = int(f.readline())
         a = f.readline().strip().split(',')
-        #a = [parse_line(line) for line in f] 
+
         print(x, a[:10])
 
         print(f'part one: {p1(x, a)}')
