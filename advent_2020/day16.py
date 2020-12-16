b1cd1215af6a59fae5be1c64a2fbbfd37b7f8fda
justinxu421 <justinx@stanford.edu>
2020-12-16 01:52:08 -0500

    a little cleaner

--- advent_2020/day16.py
+++ advent_2020/day16.py
@@ -1,103 +1,95 @@
 from collections import Counter, defaultdict
-from utils import print_d
 
-def parse_rule(f):
-    low,high = f.split('-')
-    return (int(low), int(high))
+def get_rule_dict(rules):
+    rule_dict = {}
 
-def check(all_rules, entry):
-    for low, high in all_rules:
-        if entry >= low and entry <=  high:
+    def parse_rule(f):
+        low,high = f.split('-')
+        return (int(low), int(high))
+
+    for field, rng in rules:
+        f1, f2 = rng.split(' or ')
+        rule_dict[field] = (parse_rule(f1), parse_rule(f2))
+
+    return rule_dict
+
+def check_key(rule_dict, key, entry):
+    f1, f2 = rule_dict[key]
+    return f1[0] <= entry <= f1[1] or f2[0] <= entry <= f2[1]
+
+def check(rule_dict, entry):
+    for key in rule_dict:
+        if check_key(rule_dict, key, entry):
             return True
     return False
 
-def get_rules(rules):
-    rule_dict = {}
-    all_rules = []
-    for field, rng in rules:
-        f1, f2 = rng.split(' or ')
-        all_rules.append(parse_rule(f1))
-        all_rules.append(parse_rule(f2))
-        rule_dict[field] = (parse_rule(f1), parse_rule(f2))
-    return all_rules, rule_dict
+def p1(rules, nearby):
+    rule_dict = get_rule_dict(rules)
 
-def p1(rules, tickets, nearby):
-    all_rules, rule_dict = get_rules(rules)
     total = 0
-    for i, ticket in enumerate(nearby):
+    for ticket in nearby:
         for entry in ticket:
-            if not check(all_rules, int(entry)):
-                total += int(entry)
+            if not check(rule_dict, entry):
+                total += entry
     return total
 
-def check_valid_ticket(ticket, all_rules):
+def check_valid_ticket(ticket, rule_dict):
     for entry in ticket:
-        if not check(all_rules, int(entry)):
+        if not check(rule_dict, entry):
             return False
     return True
 
-def get_valid_tickets(rules, nearby):
-    all_rules, rule_dict = get_rules(rules)
-    return [ticket for ticket in nearby if check_valid_ticket(ticket, all_rules)]
+def get_possible(ticket, rule_dict):
+    possible = []
+    
+    for entry in ticket:
+        p = set()
+        for key in rule_dict:
+            if check_key(rule_dict, key, entry):
+                p.add(key)
+        possible.append(p)
 
-def check_f(f1, entry):
-    low, high = f1
-    return  entry >= low and entry <= high
-
-def get_possible(entry, rule_dict, keys):
-    p = set()
-    for key in keys:
-        f1, f2 = rule_dict[key]
-        if check_f(f1, entry) or check_f(f2, entry):
-            p.add(key)
-    return p
+    return possible
 
 def p2(rules, ticket, nearby):
-    all_rules, rule_dict = get_rules(rules)
-    valid_tickets = get_valid_tickets(rules, nearby)
+    rule_dict = get_rule_dict(rules)
+    valid_tickets = [ticket for ticket in nearby if check_valid_ticket(ticket, rule_dict)]
     
-    keys = list(rule_dict.keys())
-
-    possible = [get_possible(int(entry), rule_dict, keys) for entry in ticket]
+    possible = get_possible(ticket, rule_dict) 
     for t in valid_tickets:
-        print([len(a) for a in possible])
-        new_possible = [get_possible(int(entry), rule_dict, keys) for entry in t]
+        new_possible = get_possible(t, rule_dict) 
         possible = [a & b for a,b in zip(possible, new_possible)]
     
-    skip = set()
+    # prune possible by recursively removing the keys with only 1 match
+    parsed = set()
     for _ in range(20):
         remove = None
         for i, a in enumerate(possible):
-            if len(a) == 1 and list(a)[0] not in skip:
+            if len(a) == 1 and i not in parsed:
                 remove = a
-                skip.add(list(a)[0])
+                parsed.add(i)
                 break
         possible = [a-remove if j!=i else a for j,a in enumerate(possible)]
-
-    print([len(a) for a in possible])
-    for i,a in enumerate(possible):
-        print(i, a)
-
+    
     total = 1
-    for idx in [3, 6, 13, 14, 15, 18]:
-        total *= int(ticket[idx])
-        print(ticket[idx])
+    for idx, key in enumerate(possible):
+        if 'departure' in list(key)[0]:
+            total *= ticket[idx]
     return total
 
-### insert how to parse line
-def parse_line(line):
-    return list(line.strip())
+def parse_ticket(line):
+    return [int(x) for x in line.strip().split(',')]
 
 def main():
     with open('input16.txt') as f:
-        rules, tickets, nearby = f.read().split('\n\n')
+        rules, tick, nearby_ticks = f.read().split('\n\n')
 
         rules = list(map(lambda x: x.split(': '), rules.split('\n')))
-        tickets = tickets.split(':\n')[1].split(',')
-        nearby = list(map(lambda x: x.split(','), nearby.split(':\n')[1].strip().split('\n')))
+        tick = parse_ticket(tick.split(':\n')[1])
+        nearby_ticks = [parse_ticket(x) for x in nearby_ticks.split(':\n')[1].strip().split('\n')]
         
-        print(f'part one: {p1(rules, tickets, nearby)}')
-        print(f'part two: {p2(rules, tickets, nearby)}')
+        print(f'part one: {p1(rules, nearby_ticks)}')
+        print(f'part two: {p2(rules, tick, nearby_ticks)}')
 
 if __name__ == "__main__":
     main()
