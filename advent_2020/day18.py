404481b316e8d0a12aa0b411622e5fec554e50b9
justinxu421 <justinx@stanford.edu>
2020-12-18 01:04:42 -0500

    clean up 18

--- advent_2020/day18.py
+++ advent_2020/day18.py
@@ -1,62 +1,55 @@
 from collections import Counter, defaultdict
 from utils import print_d
 
-def evaluate(eq):
+def simplify(string):
+    return str(eval(''.join(string)))
+
+def evaluate(eq, i, f):
     string = []
-    while eq:
-        cur = eq.pop(0)
-
+    while i < len(eq):
+        cur = eq[i]
+        i += 1
+        
+        # exit out of recursion
         if cur == ')':
-            return str(eval(''.join(string)))
-
+            return simplify(string), i
+            
+        # recursively evaluate
         if cur == '(':
-            string.append(evaluate(eq))
+            res, i = evaluate(eq, i, f)
+            string.append(res)
         else:
             string.append(cur)
-
-        if len(string) == 3:
-            string = [str(eval(''.join(string)))]
+        
+        # simplify if length of string is 3
+        string = f(string)
     
-    return str(eval(''.join(string)))
+    return simplify(string), i
+
+def naive(string):
+    if len(string) == 3:
+        string = [simplify(string)]
+    return string
 
 def p1(a):
     total = 0
     for eq in a:
-        total += int(evaluate(eq))
+        total += int(evaluate(eq, 0, naive)[0])
     return total
 
-def evaluate_2(eq):
-    print(''.join(eq))
-
-    string = []
-    while eq:
-        cur = eq.pop(0)
-        print(f'string: {string}, cur: {cur}')
-
-        if cur == ')':
-            return str(eval(''.join(string)))
-
-        if cur == '(':
-            string.append(evaluate_2(eq))
-        else:
-            string.append(cur)
-
-        if len(string) >= 3 and string[-2] == '+':
-            exp = string[-3:]
-            for _ in range(3):
-                string.pop()
-            string.append(str(eval(''.join(exp))))
-            '''
-            if string[1] == '+':
-                string = [str(eval(''.join(string)))]
-            '''
-    
-    return str(eval(''.join(string)))
+# + takes precendence over *
+def precedence(string):
+    # simplify only if last 3 are '+'
+    if len(string) >= 3 and string[-2] == '+':
+        exp = string[-3:]
+        string = string[:-3]
+        string.append(simplify(exp))
+    return string
 
 def p2(a):
     total = 0
     for eq in a:
-        total += int(evaluate_2(eq))
+        total += int(evaluate(eq, 0, precedence)[0])
     return total
 
 ### insert how to parse line
@@ -66,8 +59,8 @@ def parse_line(line):
 def main():
     with open('input18.txt') as f:
         a = [parse_line(line) for line in f] 
-        #print(f'part one: {p1(a[:])}')
-        print(f'part two: {p2(a[:])}')
+        print(f'part one: {p1(a)}')
+        print(f'part two: {p2(a)}')
 
 if __name__ == "__main__":
     main()
