c453524f2b9508033fbfbc401554b56ba83faa6e
justinxu421 <justinx@stanford.edu>
2020-12-18 02:37:09 -0500

    parsing without eval day 18

--- advent_2020/day18.py
+++ advent_2020/day18.py
@@ -1,9 +1,6 @@
 from collections import Counter, defaultdict
 from utils import print_d
 
-def simplify(string):
-    return str(eval(''.join(string)))
-
 def evaluate(eq, i, f):
     string = []
     while i < len(eq):
@@ -12,7 +9,7 @@ def evaluate(eq, i, f):
         
         # exit out of recursion
         if cur == ')':
-            return simplify(string), i
+            return f(string), i
             
         # recursively evaluate
         if cur == '(':
@@ -20,36 +17,46 @@ def evaluate(eq, i, f):
             string.append(res)
         else:
             string.append(cur)
-        
-        # simplify if length of string is 3
-        string = f(string)
     
-    return simplify(string), i
+    return f(string), i
 
 def naive(string):
-    if len(string) == 3:
-        string = [simplify(string)]
-    return string
+    a = int(string[0])
+   
+    i = 1
+    while i < len(string):
+        b = int(string[i+1])
+        if string[i] == '*':
+            a *= b
+        elif string[i] == '+':
+            a += b
+        i += 2
+    return a
 
 def p1(a):
     total = 0
     for eq in a:
-        total += int(evaluate(eq, 0, naive)[0])
+        total += evaluate(eq, 0, naive)[0]
     return total
 
 # + takes precendence over *
 def precedence(string):
-    # simplify only if last 3 are '+'
-    if len(string) >= 3 and string[-2] == '+':
-        exp = string[-3:]
-        string = string[:-3]
-        string.append(simplify(exp))
-    return string
+    a = int(string[0])
+
+    i = 1
+    while i < len(string):
+        b = int(string[i+1])
+        if string[i] == '*':
+            return a * precedence(string[i+1:]) 
+        elif string[i] == '+':
+            a += b
+        i += 2
+    return a
 
 def p2(a):
     total = 0
     for eq in a:
-        total += int(evaluate(eq, 0, precedence)[0])
+        total += evaluate(eq, 0, precedence)[0]
     return total
 
 ### insert how to parse line
