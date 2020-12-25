ccd00b1c18d87b3f5a83e9c704a3a92dcab4a220
justinxu421 <justinx@stanford.edu>
2020-12-25 00:48:43 -0500

    straight up loop

--- advent_2020/day25.py
+++ advent_2020/day25.py
@@ -4,33 +4,25 @@ from utils import print_d
 div = 20201227
 
 memo = {}
-def transform(subject, loop):
-    if loop == 0:
-        memo[subject, loop] = 1
-    elif (subject, loop) not in memo:
-        memo[subject, loop] = subject * memo[subject, loop-1] % div
-    return memo[subject, loop]
+def transform(subject, loop, end = None):
+    start = 1
+    for i in range(loop):
+        if end and start == end:
+            return i, end
+        start = start * subject % div
+    return i, start
 
 def p1(a):
-    memo = {}
-    for i in range(100000000):
-        key = transform(7, i)
-        if key == a[0]:
-            card = i
-            print(card, key)
-        if key == a[1]:
-            door = i
-            print(door, key)
-            break
-    
-    print(card, door)
+    print(f'array is {a}\n')
+    card, x  = transform(7, 100000000, a[0])
+    print(f'card is {card}, {x}')
+    door, y  = transform(7, 100000000, a[1])
+    print(f'door is {door}, {y}\n')
 
-    for i in range(door+1):
-        a1 = transform(a[0], i)
-    print(i, a1)
-    for i in range(card+1):
-        a2 = transform(a[1], i)
-    print(i, a2)
+    _, a1 = transform(a[0], door)
+    print(f'a1 is {a1}')
+    _, a2 = transform(a[1], card)
+    print(f'a2 is {a2}')
 
     return a1, a2
 
@@ -41,9 +33,7 @@ def parse_line(line):
 def main():
     with open('input25.txt') as f:
         a = [parse_line(line) for line in f] 
-
         print(f'part one: {p1(a)}')
-        print(f'part two: {p2(a)}')
 
 if __name__ == "__main__":
     main()
