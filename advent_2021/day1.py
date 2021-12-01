a004dc63371b183df3f4202029b833851133b3bb
Justin Xu <justin.xu@klaviyo.com>
2021-12-01 11:18:17 -0500

    raw python day 1

--- advent_2021/day1.py
+++ advent_2021/day1.py
@@ -1,16 +1,21 @@
 import numpy as np
 import pandas as pd
+from functools import reduce
 
 
 def p1(a):
-    diffs = np.array(a[1:]) - np.array(a[:-1])
-    return (diffs > 0).sum()
+    diffs = map(lambda x: x[0] - x[1], zip(a[1:], a[:-1]))
+    return sum([x > 0 for x in diffs])
+
+
+def rolling3(a):
+    tuples = zip(a[:-2], a[1:-1], a[2:])
+    return map(lambda x: x[0] + x[1] + x[2], tuples)
 
 
 def p2(a):
-    b = pd.Series(a).rolling(3).sum().dropna()
-    diffs = np.array(b[1:]) - np.array(b[:-1])
-    return (diffs > 0).sum()
+    diffs = map(lambda x: x[0] - x[1], zip(rolling3(a[1:]), rolling3(a[:-1])))
+    return sum([x > 0 for x in diffs])
 
 
 with open("input1.txt") as f:
