73bec1a5c212f1102d7333794def6baf51dff865
Justin Xu <jxu421@gmail.com>
2021-12-06 01:29:32 -0500

    include better and cetter versions of day 6

--- advent_2021/day6.py
+++ advent_2021/day6.py
@@ -31,11 +31,38 @@ def p2(a):
         total += 1 + x
     return total
 
+def p2b(a):
+    fish_counts = Counter(a)
+    for _ in range(256):
+        new_counts = Counter()
+        for timer, count in fish_counts.items():
+            if timer == 0:
+                new_counts[6] += count
+                new_counts[8] += count
+            else:
+                new_counts[timer - 1] += count
+        fish_counts = new_counts
+    return sum(fish_counts.values())
+
+def p2c(a):
+    fish_counts = np.zeros(9)
+    for num in a:
+        fish_counts[num] += 1
+
+    for _ in range(256):
+        zeros = fish_counts[0]
+        fish_counts = np.append(fish_counts[1:], 0)
+        fish_counts[6] += zeros
+        fish_counts[8] += zeros
+    return int(fish_counts.sum())
+
 def run(file):
     with open(file) as f:
         a = list(map(int, f.read().strip().split(',')))
         print(f"Part 1: {p1(a)}")
         print(f"Part 2: {p2(a)}")
+        print(f"Part 2 better: {p2b(a)}")
+        print(f"Part 2 cetter: {p2c(a)}")
 
 
 print('Run test cases')
