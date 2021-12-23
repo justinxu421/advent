2a00c654c62a3b1f45701cfac682f05f574d2961
Justin Xu <jxu421@gmail.com>
2021-12-23 12:33:39 -0500

    day 23

--- advent_2021/day23.py
+++ advent_2021/day23.py
@@ -3,22 +3,7 @@ from functools import cache
 import numpy as np
 import copy
 
-
-def get_path(positions, space, room):
-    x1, y1 = space
-    x2, y2 = room
-
-    for y in range(min(y1, y2) + 1, max(y1, y2)):
-        if positions.get((x1, y), ".") != ".":
-            return False
-
-    for x in range(x1 + 1, x2):
-        if positions.get((x, y2), ".") != ".":
-            return False
-
-    return abs(y2 - y1) + abs(x2 - x1)
-
-
+# make a constant for the spaces that are able to be moved to
 SPACES = [
     (1, 1),
     (1, 2),
@@ -29,7 +14,26 @@ SPACES = [
     (1, 11),
 ]
 
+# all paths are L shaped, so we just traverse from space to room
+# space is always on a row higher than the room, so we can make that assumption
+def get_path(positions, space, room):
+    x1, y1 = space
+    x2, y2 = room
 
+    # horizontal
+    for y in range(min(y1, y2) + 1, max(y1, y2)):
+        if positions.get((x1, y), ".") != ".":
+            return False
+
+    # vertical
+    for x in range(x1 + 1, x2):
+        if positions.get((x, y2), ".") != ".":
+            return False
+
+    return abs(y2 - y1) + abs(x2 - x1)
+
+
+# check if all positions are filled
 def check_done(positions, room_mapping):
     for label, rooms in room_mapping.items():
         for room in rooms:
@@ -39,6 +43,7 @@ def check_done(positions, room_mapping):
     return True
 
 
+# find all unfilled rooms that are movable
 def get_unfilled(pos, room_mapping):
     def get_first(rooms):
         for start in rooms:
@@ -58,7 +63,8 @@ def get_unfilled(pos, room_mapping):
     return valid
 
 
-def check_spaces(p_, scores, room_mapping):
+# get all the valid moves that go from space to room
+def move_space_to_room(p_, scores, room_mapping):
     # see if any space can go into any room
     new_ps = []
     for space in SPACES:
@@ -87,6 +93,29 @@ def check_spaces(p_, scores, room_mapping):
     return new_ps
 
 
+# get all the valid moves that go from room to unfilled space
+def move_room_to_unfilled(p_, scores, room_mapping):
+    new_ps = []
+    # check all valid pods
+    for r in get_unfilled(p_, room_mapping):
+        pod = p_[r]
+        assert pod != "."
+        # check all valid spaces
+        for space in SPACES:
+            if p_[space] == ".":
+                path_len = get_path(p_, space, r)
+                if path_len:
+                    pass
+                    p_copy = copy.deepcopy(p_)
+                    # move the pod to the space
+                    p_copy[space] = pod
+                    p_copy[r] = "."
+
+                    new_ps.append((p_copy, scores[pod] * path_len))
+
+    return new_ps
+
+
 def get_smallest_path(positions, room_mapping):
     scores = {"A": 1, "B": 10, "C": 100, "D": 1000}
     min_total = float("inf")
@@ -105,49 +134,35 @@ def get_smallest_path(positions, room_mapping):
                 print(f"new min total is {min_total}")
 
         # if we can move do into room do it
-        new_ps = check_spaces(p_, scores, room_mapping)
-        if new_ps:
-            for p_copy, move_val in new_ps:
-                # print("moving", p_copy)
-                new_total = total_ + move_val
-                if str(p_copy) not in seen or new_total < seen[str(p_copy)]:
-                    seen[str(p_copy)] = new_total
-                    stack.append((p_copy, new_total))
+        room_ps = move_space_to_room(p_, scores, room_mapping)
+        unfilled_ps = move_room_to_unfilled(p_, scores, room_mapping)
 
+        if room_ps:
+            new_ps = room_ps
         else:
-            # check all valid pods
-            for r in get_unfilled(p_, room_mapping):
-                pod = p_[r]
-                assert pod != "."
-                # check all valid spaces
-                for space in SPACES:
-                    if p_[space] == ".":
-                        path_len = get_path(p_, space, r)
-                        if path_len:
-                            pass
-                            p_copy = copy.deepcopy(p_)
-                            # move the pod to the space
-                            p_copy[space] = pod
-                            p_copy[r] = "."
+            new_ps = unfilled_ps
 
-                            new_total = total_ + scores[pod] * path_len
-
-                            # already seen a path that gets there in less steps
-                            if str(p_copy) not in seen or new_total < seen[str(p_copy)]:
-                                seen[str(p_copy)] = new_total
-                                stack.append((p_copy, total_ + scores[pod] * path_len))
+        for p_copy, move_val in new_ps:
+            new_total = total_ + move_val
+            if str(p_copy) not in seen or new_total < seen[str(p_copy)]:
+                seen[str(p_copy)] = new_total
+                stack.append((p_copy, new_total))
 
     return min_total
 
 
-def p1(a, room_1, room_2, room_3, room_4):
+def init_positions(a, rows=2):
     positions = {}
     for coord in SPACES:
-        positions[coord] = a[coord]
+        positions[coord] = "."
+
+    room_1 = [(x, 3) for x in range(2, 2 + rows)]
+    room_2 = [(x, 5) for x in range(2, 2 + rows)]
+    room_3 = [(x, 7) for x in range(2, 2 + rows)]
+    room_4 = [(x, 9) for x in range(2, 2 + rows)]
 
     for coord in room_1 + room_2 + room_3 + room_4:
         positions[coord] = a[coord]
-    print(positions, "\n")
 
     room_mapping = {
         "A": room_1,
@@ -156,6 +171,11 @@ def p1(a, room_1, room_2, room_3, room_4):
         "D": room_4,
     }
 
+    return positions, room_mapping
+
+
+def p1(a):
+    positions, room_mapping = init_positions(a, 2)
     return get_smallest_path(positions, room_mapping)
 
 
@@ -163,16 +183,11 @@ def p2(a):
     row1 = "  #D#C#B#A#  "
     row2 = "  #D#B#A#C#  "
     rows = np.array([list(row1), list(row2)])
-
     b = np.insert(a, 3, rows, 0)
     print(b)
 
-    room_1 = [(2, 3), (3, 3), (4, 3), (5, 3)]
-    room_2 = [(2, 5), (3, 5), (4, 5), (5, 5)]
-    room_3 = [(2, 7), (3, 7), (4, 7), (5, 7)]
-    room_4 = [(2, 9), (3, 9), (4, 9), (5, 9)]
-
-    return p1(b, room_1, room_2, room_3, room_4)
+    positions, room_mapping = init_positions(b, 4)
+    return get_smallest_path(positions, room_mapping)
 
 
 def run(file):
@@ -180,13 +195,7 @@ def run(file):
         a = [list(x) for x in f.read().splitlines()]
         a = np.array(a)
 
-        room_1 = [(2, 3), (3, 3)]
-        room_2 = [(2, 5), (3, 5)]
-        room_3 = [(2, 7), (3, 7)]
-        room_4 = [(2, 9), (3, 9)]
-
-        print(f"Part 1: {p1(a, room_1, room_2, room_3, room_4)}")
-
+        print(f"Part 1: {p1(a)}")
         print(f"Part 2: {p2(a)}")
 
 
