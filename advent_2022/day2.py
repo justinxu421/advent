d981f0bdc84e3b480493275f19a232d773c0a7cc
Justin Xu <jxu421@gmail.com>
2022-12-01 21:30:39 -0800

    cleanup

--- advent_2022/day2.py
+++ advent_2022/day2.py
@@ -1,55 +1,38 @@
-from collections import Counter, defaultdict
-import numpy as np
-
-win_map = {
-    "A": "Y",
-    "B": "Z",
-    "C": "X",
-}
-draw = {"A": "X", "B": "Y", "C": "Z"}
-
-point_map = {"X": 1, "Y": 2, "Z": 3}
-
-
 def p1(a):
     points = 0
     for x in a:
         elf, me = x.split()
-        points += point_map[me]
-        if win_map[elf] == me:
+        p1 = ord(elf) - ord("A") + 1
+        p2 = ord(me) - ord("X") + 1
+        points += p2
+
+        if (p2 - p1) % 3 == 1:
             points += 6
-        elif draw[elf] == me:
+        if p2 == p1:
             points += 3
+
     return points
 
 
 def p2(a):
     points = 0
     for x in a:
-        elf, s_ = x.split()
+        elf, state = x.split()
 
         # 0/1/2
         p1 = ord(elf) - ord("A")
-        # -1/0/1
-        state = ord(s_) - ord("X") - 1
 
-        # lose
-        if state == -1:
+        if state == "X":
             p2 = (p1 - 1) % 3
-        if state == 0:
+        if state == "Y":
             p2 = p1
-        if state == 1:
-            p2 = (p1 + 1) % 3
-        me = chr(p2 + ord("X"))
-
-        points += point_map[me]
-        if state == 1:
-            points += 6
-        elif state == 0:
             points += 3
-    return points
+        if state == "Z":
+            p2 = (p1 + 1) % 3
+            points += 6
 
-    pass
+        points += p2 + 1
+    return points
 
 
 def run(file):
